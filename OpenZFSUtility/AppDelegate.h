7329de244213e47abd74249a90314a2224e98797
Silentzzz <silentzzz@yandex.ru>
2015-02-02 14:04:03 +0300

    Initial Commit

---
+++ OpenZFSUtility/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenZFSUtility
+//
+//  Created by Dmitry on 02/02/15.
+//  Copyright (c) 2015 Studiolaundry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OpenZFSUtility/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OpenZFSUtility
+//
+//  Created by Dmitry on 02/02/15.
+//  Copyright (c) 2015 Studiolaundry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
